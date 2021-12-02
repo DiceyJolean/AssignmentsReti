8459be71b480330e0a85d851c1af055c4f91614b
DiceyJolean <87235846+DiceyJolean@users.noreply.github.com>
2021-12-02 20:41:36 +0100

    Add files via upload

---
+++ Ass10/Server.java
@@ -0,0 +1,56 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Server{
+    private static int port = 6789;
+    private static int toSleep = 500;
+
+    public static void main( String[] args ){
+        InetAddress address;
+
+        try{
+            // Genero l'indirizzo IP di multicast
+            address = InetAddress.getByName(args[0]);
+            if ( !address.isMulticastAddress() ){
+                System.err.printf("SERVER: L'indirizzo non è un Multicast Address\n");
+                return;
+            }
+        } catch ( Exception e ){
+            System.err.printf("SERVER: Errore nella creazione dell'indirizzo IP - %s\n", e.getMessage());
+            return;
+        }
+        
+        try(
+            // Instauro la connessione UDP
+            DatagramSocket socket = new DatagramSocket(port);
+        ){
+            while ( Thread.currentThread().isInterrupted() ){
+
+                // Preparo il messaggio da spedire
+                DateFormat format = new SimpleDateFormat("dd/MM/yyy HH:mm:ss");
+                Date date = Calendar.getInstance().getTime();
+                byte[] buf = format.format(date).getBytes();
+
+                // Preparo il datagramma UDP
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+
+                // Invio il pacchetto
+                socket.send(packet);
+
+                // Attendo un intervallo di tempo periodico tra un invio e l'altro
+                Thread.sleep(toSleep); // InterruptedException gestita dal catch generico per Exception
+
+            }
+        } catch ( Exception e ){
+            System.err.printf("SERVER: Errore - %s\n", e.getMessage());
+            return;
+        }
+    }
+
+}
